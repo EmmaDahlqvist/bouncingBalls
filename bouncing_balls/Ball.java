86af5410c89afae01000b60ba31bb5f1c3f4420d
aishamohamed <aishac@chalmers.se>
2025-09-28 00:31:25 +0200

    Some fixes

---
+++ bouncing_balls/Ball.java
@@ -0,0 +1,14 @@
+package bouncing_balls;
+public class Ball {
+
+    double x, y, vx, vy, radius, m;
+
+    Ball(double x, double y, double vx, double vy, double r, double m) {
+        this.x = x; 
+        this.y = y; 
+        this.vx = vx; 
+        this.vy = vy; 
+        this.radius = r; 
+        this.m = m;
+    }
+}
