92ddbb6f13878f0202153628152ac9aa2d7201c1
aishamohamed <aishac@chalmers.se>
2025-09-28 00:28:42 +0200

    Some fixes

--- bouncing_balls/Model.java
+++ bouncing_balls/Model.java
@@ -171,19 +171,5 @@ class Model {
             b2.y += separation * Math.sin(angle);
         }
     }
-
-    class Ball {
-
-        double x, y, vx, vy, radius, m;
-
-        Ball(double x, double y, double vx, double vy, double r, double m) {
-            this.x = x; 
-            this.y = y; 
-            this.vx = vx; 
-            this.vy = vy; 
-            this.radius = r; 
-            this.m = m;
-        }
-    }
     
 }
